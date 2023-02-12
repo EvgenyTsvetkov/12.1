723977200c08a124ceaf108cb0f358e2bc7c91c4
EvgenyTsvetkov <108621064+EvgenyTsvetkov@users.noreply.github.com>
2023-02-12 19:22:50 +0200

    COmmit

---
+++ src/main/java/Billboard.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+
+public class Billboard {
+
+    private int id;
+    private String name;
+    private String genre;
+    private String Url;
+    private boolean Announcement;
+}
