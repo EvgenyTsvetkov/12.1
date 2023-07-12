27df07c60e28fa25b73e5f96e9a172b32520b481
EvegenyTsvetkov <geniatsvetkov@gmail.com>
2023-07-12 23:34:10 +0200

    NoCycles commit

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ManagerTest {
@@ -28,31 +30,26 @@ public class ManagerTest {
         Movie[] expected = new Movie[]{movie1, movie2};
         assertArrayEquals(expected, actual);
     }
-
-    @Test
-    void findLastWithLimitLessThanMovies() {
-        for (int i = 0; i < 11; i++) {
-            manager.add(new Movie("Movie" + i));
-        }
-        Movie[] lastMovies = manager.findLast();
-        assertEquals(10, lastMovies.length);
-        assertEquals("Movie10", lastMovies[0].getTitle());
-    }
-
-    @Test
-    void findLastWithLimitMoreThanMovies() {
-        Manager customManager = new Manager(15);
-        for (int i = 0; i < 11; i++) {
-            customManager.add(new Movie("Movie" + i));
-        }
-        Movie[] lastMovies = customManager.findLast();
-        assertEquals(11, lastMovies.length);
-        assertEquals("Movie10", lastMovies[0].getTitle());
-    }
-
     @Test
     void findLastWithNoMovies() {
         Movie[] lastMovies = manager.findLast();
         assertEquals(0, lastMovies.length);
     }
+    @Test
+    void findLastWithLimitLessThanMovies() {
+        IntStream.rangeClosed(1, 11).mapToObj(i -> new Movie("Movie" + i)).forEach(manager::add);
+        Movie[] lastMovies = manager.findLast();
+        assertEquals(10, lastMovies.length);
+        assertEquals("Movie11", lastMovies[0].getTitle());
+    }
+
+    @Test
+    void findLastWithLimitMoreThanMovies() {
+        Manager customManager = new Manager(15);
+        IntStream.rangeClosed(1, 11).mapToObj(i -> new Movie("Movie" + i)).forEach(customManager::add);
+        Movie[] lastMovies = customManager.findLast();
+        assertEquals(11, lastMovies.length);
+        assertEquals("Movie11", lastMovies[0].getTitle());
+    }
+
 }
